3e907c3b8f27b7f38cb3b4ec6ace4b4c9eb21905
Pedro Bueno <pedrolbb.e@gmail.com>
2024-04-08 11:10:22 -0300

    Release v3.2.7 (#4)
    * fix: updated @expo/config-plugins
    
    * refactor: removed old example app
    
    * refactor: dev enviroment files
    
    * style: enforce code-style
    
    * test: add jest
    
    * ci: add workflows
    
    * revert: add back @tsconfig/node10
    
    * style: enforce code-style in plugins
    
    * fix: types
    
    * chore: add example to the npmignore file
    
    * feat: add example
    
    * fix: tsconfig & add sync script
    
    * refactor: removed example/

--- plugin/src/withVoice.ts
+++ plugin/src/withVoice.ts
@@ -1,28 +1,28 @@
-import {
-  AndroidConfig,
-  ConfigPlugin,
-  createRunOncePlugin,
-  withInfoPlist,
-} from '@expo/config-plugins';
+import { AndroidConfig, createRunOncePlugin, withInfoPlist, type ConfigPlugin } from '@expo/config-plugins'
 
-const pkg = require('@pedrol2b/react-native-voice/package.json');
+interface Package {
+  name: string
+  version: string
+}
 
-const MICROPHONE = 'Allow $(PRODUCT_NAME) to access the microphone';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pkg = require('@pedrol2b/react-native-voice/package.json') as Package
 
-const SPEECH_RECOGNITION =
-  'Allow $(PRODUCT_NAME) to securely recognize user speech';
+const MICROPHONE = 'Allow $(PRODUCT_NAME) to access the microphone'
 
-export type Props = {
+const SPEECH_RECOGNITION = 'Allow $(PRODUCT_NAME) to securely recognize user speech'
+
+export interface Props {
   /**
    * `NSSpeechRecognitionUsageDescription` message.
    */
-  speechRecognitionPermission?: string | false;
+  speechRecognitionPermission?: string | false
 
   /**
    * `NSMicrophoneUsageDescription` message.
    */
-  microphonePermission?: string | false;
-};
+  microphonePermission?: string | false
+}
 
 /**
  * Adds `NSMicrophoneUsageDescription` and `NSSpeechRecognitionUsageDescription` to the `Info.plist`.
@@ -31,44 +31,36 @@ export type Props = {
  * @param props.microphonePermission microphone permission message
  * @returns
  */
-const withIosPermissions: ConfigPlugin<Props> = (
-  c,
-  { microphonePermission, speechRecognitionPermission } = {},
-) => {
-  return withInfoPlist(c, config => {
+const withIosPermissions: ConfigPlugin<Props> = (c, { microphonePermission, speechRecognitionPermission } = {}) => {
+  return withInfoPlist(c, (config) => {
     if (microphonePermission !== false) {
       config.modResults.NSMicrophoneUsageDescription =
-        microphonePermission ||
-        config.modResults.NSMicrophoneUsageDescription ||
-        MICROPHONE;
+        microphonePermission ?? config.modResults.NSMicrophoneUsageDescription ?? MICROPHONE
     }
     if (speechRecognitionPermission !== false) {
       config.modResults.NSSpeechRecognitionUsageDescription =
-        speechRecognitionPermission ||
-        config.modResults.NSSpeechRecognitionUsageDescription ||
-        SPEECH_RECOGNITION;
+        speechRecognitionPermission ?? config.modResults.NSSpeechRecognitionUsageDescription ?? SPEECH_RECOGNITION
     }
 
-    return config;
-  });
-};
+    return config
+  })
+}
 
 /**
  * Adds the following to the `AndroidManifest.xml`: `<uses-permission android:name="android.permission.RECORD_AUDIO" />`
  */
-const withAndroidPermissions: ConfigPlugin = config => {
-  return AndroidConfig.Permissions.withPermissions(config, [
-    'android.permission.RECORD_AUDIO',
-  ]);
-};
+const withAndroidPermissions: ConfigPlugin = (config) => {
+  return AndroidConfig.Permissions.withPermissions(config, ['android.permission.RECORD_AUDIO'])
+}
 
-const withVoice: ConfigPlugin<Props | void> = (config, props = {}) => {
-  const _props = props ? props : {};
-  config = withIosPermissions(config, _props);
+const withVoice: ConfigPlugin<Props> = (config, props = {}) => {
+  const _props = props ?? {}
+  config = withIosPermissions(config, _props)
   if (_props.microphonePermission !== false) {
-    config = withAndroidPermissions(config);
+    config = withAndroidPermissions(config)
   }
-  return config;
-};
 
-export default createRunOncePlugin(withVoice, pkg.name, pkg.version);
+  return config
+}
+
+export default createRunOncePlugin(withVoice, pkg.name, pkg.version)
--- src/VoiceModuleTypes.ts
+++ src/VoiceModuleTypes.ts
@@ -1,88 +1,89 @@
-import { EventSubscriptionVendor } from 'react-native';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { type EventSubscription } from 'react-native'
 
-type Callback = (error: string) => void;
+type Callback = (error: string) => void
 
 export type VoiceModule = {
   /**
    * Gets list of SpeechRecognitionServices used.
    * @platform android
    */
-  getSpeechRecognitionServices: () => Promise<string[]> | void;
-  destroySpeech: (callback: Callback) => void;
-  destroyTranscription: (callback: Callback) => void;
-  startSpeech: Function;
-  startTranscription: Function;
-  stopSpeech: (callback: Callback) => void;
-  stopTranscription: (callback: Callback) => void;
-  cancelSpeech: (callback: Callback) => void;
-  cancelTranscription: (callback: Callback) => void;
-  isRecognizing: Function;
-  isSpeechAvailable: Function;
+  getSpeechRecognitionServices: () => Promise<string[]>
+  destroySpeech: (callback: Callback) => void
+  destroyTranscription: (callback: Callback) => void
+  startSpeech: (locale: any, options?: any) => Promise<void>
+  startTranscription: (url: any, locale: any, options?: any) => Promise<void>
+  stopSpeech: (callback: Callback) => void
+  stopTranscription: (callback: Callback) => void
+  cancelSpeech: (callback: Callback) => void
+  cancelTranscription: (callback: Callback) => void
+  isRecognizing: () => Promise<0 | 1>
+  isSpeechAvailable: () => Promise<0 | 1>
 } & SpeechEvents &
   TranscriptionEvents &
-  EventSubscriptionVendor;
+  EventSubscription
 
-export type SpeechEvents = {
-  onSpeechStart?: (e: SpeechStartEvent) => void;
-  onSpeechRecognized?: (e: SpeechRecognizedEvent) => void;
-  onSpeechEnd?: (e: SpeechEndEvent) => void;
-  onSpeechError?: (e: SpeechErrorEvent) => void;
-  onSpeechResults?: (e: SpeechResultsEvent) => void;
-  onSpeechPartialResults?: (e: SpeechResultsEvent) => void;
-  onSpeechVolumeChanged?: (e: SpeechVolumeChangeEvent) => void;
-};
+export interface SpeechEvents {
+  onSpeechStart?: (e: SpeechStartEvent) => void
+  onSpeechRecognized?: (e: SpeechRecognizedEvent) => void
+  onSpeechEnd?: (e: SpeechEndEvent) => void
+  onSpeechError?: (e: SpeechErrorEvent) => void
+  onSpeechResults?: (e: SpeechResultsEvent) => void
+  onSpeechPartialResults?: (e: SpeechResultsEvent) => void
+  onSpeechVolumeChanged?: (e: SpeechVolumeChangeEvent) => void
+}
 
-export type TranscriptionEvents = {
-  onTranscriptionStart?: (e: TranscriptionStartEvent) => void;
-  onTranscriptionEnd?: (e: TranscriptionEndEvent) => void;
-  onTranscriptionError?: (e: TranscriptionErrorEvent) => void;
-  onTranscriptionResults?: (e: TranscriptionResultsEvent) => void;
-};
+export interface TranscriptionEvents {
+  onTranscriptionStart?: (e: TranscriptionStartEvent) => void
+  onTranscriptionEnd?: (e: TranscriptionEndEvent) => void
+  onTranscriptionError?: (e: TranscriptionErrorEvent) => void
+  onTranscriptionResults?: (e: TranscriptionResultsEvent) => void
+}
 
-export type SpeechStartEvent = {
-  error?: boolean;
-};
+export interface SpeechStartEvent {
+  error?: boolean
+}
 
-export type TranscriptionStartEvent = {
-  error?: boolean;
-};
+export interface TranscriptionStartEvent {
+  error?: boolean
+}
 
-export type SpeechRecognizedEvent = {
-  isFinal?: boolean;
-};
+export interface SpeechRecognizedEvent {
+  isFinal?: boolean
+}
 
-export type SpeechResultsEvent = {
-  value?: string[];
-};
+export interface SpeechResultsEvent {
+  value?: string[]
+}
 
-export type TranscriptionResultsEvent = {
-  segments?: string[];
-  transcription?: string;
-  isFinal?: boolean;
-};
+export interface TranscriptionResultsEvent {
+  segments?: string[]
+  transcription?: string
+  isFinal?: boolean
+}
 
-export type SpeechErrorEvent = {
+export interface SpeechErrorEvent {
   error?: {
-    code?: string;
-    message?: string;
-  };
-};
+    code?: string
+    message?: string
+  }
+}
 
-export type TranscriptionErrorEvent = {
+export interface TranscriptionErrorEvent {
   error?: {
-    code?: string;
-    message?: string;
-  };
-};
+    code?: string
+    message?: string
+  }
+}
 
-export type SpeechEndEvent = {
-  error?: boolean;
-};
+export interface SpeechEndEvent {
+  error?: boolean
+}
 
-export type TranscriptionEndEvent = {
-  error?: boolean;
-};
+export interface TranscriptionEndEvent {
+  error?: boolean
+}
 
-export type SpeechVolumeChangeEvent = {
-  value?: number;
-};
+export interface SpeechVolumeChangeEvent {
+  value?: number
+}
